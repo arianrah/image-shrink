656e435e40ac9a475e4440b1862c5425123727e6
arianrah <arianrahimi@icloud.com>
2020-11-18 15:50:16 -0800

    Added mac convention of windows closing to app.

--- main.js
+++ main.js
@@ -1,13 +1,36 @@
 const { app, BrowserWindow } = require('electron');
 
+// set env
+process.env.NODE_ENV = 'development';
+
+const isDev = process.env.NOD !== 'production' ? true : false;
+const isMac = process.platform === 'darwin' ? true : false;
+
 let mainWindow;
 
 function createMainWindow() {
 	mainWindow = new BrowserWindow({
 		title: 'Image Shrink',
 		width: 500,
-		height: 600
+		height: 600,
+		icon: `${__dirname}/assets/Icon_256x256.png`,
+		resizable: isDev ? true : false
 	});
+
+	mainWindow.loadURL(`file://${__dirname}/app/index.html`);
+	// mainWindow.loadFile('./app/index.html'); dev
 }
 
 app.on('ready', createMainWindow);
+
+app.on('window-all-closed', () => {
+	if (!isMac) {
+		app.quit();
+	}
+});
+
+app.on('activate', () => {
+	if (BrowserWindow.getAllWindows().length === 0) {
+		createMainWindow();
+	}
+});
